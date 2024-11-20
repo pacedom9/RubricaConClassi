014b827d581a9d115708f40db3a80a461d1f8d6a
pacedom9 <pacedomenico73@gmail.com>
2024-11-20 17:54:51 +0100

    Added something

--- RubricaConClassi/Contatto.cs
+++ RubricaConClassi/Contatto.cs
@@ -1,9 +1,18 @@
-﻿public class Contatto
+﻿using CsvHelper.Configuration.Attributes;
+
+public class Contatto
 {
-    private string Nome;
-    private string Cognome;
-    private string Email;
-    private string Telefono;
+    [Index(0)]
+    public string Nome { get; set; }
+
+    [Index(1)]
+    public string Cognome { get; set; }
+
+    [Index(2)]
+    public string Email { get; set; }
+
+    [Index(3)]
+    public string Telefono { get; set; }
 
     public Contatto(string nome, string cognome, string email, string telefono)
     {
@@ -11,19 +20,15 @@
         Cognome = cognome;
         Email = email;
         Telefono = telefono;
-
     }
 
     public string Get()
     {
         return $"{Nome}, {Cognome}, {Email}, {Telefono}";
     }
+
     public string GetEmail()
     {
         return $"{Email}";
     }
-
-
-
-
 }
--- RubricaConClassi/Rubrica.cs
+++ RubricaConClassi/Rubrica.cs
@@ -1,82 +1,77 @@
-﻿
-
-using System.Xml.Linq;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
 
 namespace RubricaConClassi;
 
-public class Rubrica
+public static class Rubrica
 {
-
-
-    public static void AggiungiContatto()
+    static List<Contatto> contatti = new();
+    const string percorsoFile = @"C:\Users\dpace\Desktop\rub2.csv";
+    public static List<Contatto> CaricaRubrica()
     {
-        string percorsoFile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\diecimila.csv";
-        bool emailTrovata = false;
-        Console.Write("Inserisci email dell'utente da modificare: ");
-        var ricercaEmail = Console.ReadLine();
 
-        List<string> righe = new List<string>(File.ReadAllLines(percorsoFile));
-        List<Contatto> contatti = new List<Contatto>();
+        using (var reader = new StreamReader(percorsoFile))
+        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { HeaderValidated = null, MissingFieldFound = null, HasHeaderRecord = false}))
 
-        foreach (var riga in righe)
         {
-            var colonne = riga.Split(',');
-            var contatto = new Contatto(colonne[0], colonne[1], colonne[2], colonne[3]);
-            contatti.Add(contatto);
+            var a = csv.GetRecords<Contatto>();
+            contatti = csv.GetRecords<Contatto>().ToList();
+
+        }
+        return contatti;
+
+    }
+    public static void SalvaRubrica(List<Contatto> contatti)
+    {
+        using (var writer = new StreamWriter(percorsoFile))
+        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecords(contatti);
         }
 
-        for (int i = 0; i < contatti.Count; i++)
+
+    }
+    public static void AggiungiContatto()
+    {
+        
+
+        Console.WriteLine("Inserisci nome:");
+        string? nome = Console.ReadLine();
+        Console.WriteLine("Inserisci cognome:");
+        string? cognome = Console.ReadLine();
+        Console.WriteLine("Inserisci email:");
+        string? email = Console.ReadLine();
+        Console.WriteLine("Inserisci telefono:");
+        string? telefono = Console.ReadLine();
+
+        foreach (var riga in contatti)
         {
-            var contatto = contatti[i];
-
-            if (contatto.GetEmail() == ricercaEmail)
+            if (riga.GetEmail() == email)
             {
-                emailTrovata = true;
-                Console.Write("Inserisci nuovo nome: ");
-                string? nuovoNome = Console.ReadLine();
-                Console.Write("Inserisci nuovo cognome: ");
-                string? nuovoCognome = Console.ReadLine();
-                Console.Write("Inserisci nuova email: ");
-                string? nuovaEmail = Console.ReadLine();
-                Console.Write("Inserisci nuovo numero di telefono: ");
-                string? nuovoTelefono = Console.ReadLine();
-
-                contatti[i] = new Contatto(nuovoNome, nuovoCognome, nuovaEmail, nuovoTelefono);
-
-                var nuoveRighe = contatti.Select(c => c.Get()).ToList();  // Usa Get() per ottenere la rappresentazione del contatto in formato CSV
-                File.WriteAllLines(percorsoFile, nuoveRighe);
-
-                Console.WriteLine("\nContatto modificato correttamente");
-                break;
+                Console.WriteLine("Contatto già esistente!");
+                return;
             }
         }
 
-        if (!emailTrovata)
-        {
-            Console.WriteLine("Email non trovata");
-        }
+        Contatto nuovoContatto = new Contatto(nome, cognome, email, telefono);
+        contatti.Add(nuovoContatto);
+
+        SalvaRubrica(contatti);
+
+        Console.WriteLine("Contatto aggiunto con successo!");
     }
     public static void ModificaContatto()
     {
-        string percorsoFile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\diecimila.csv";
         bool emailTrovata = false;
         Console.Write("Inserisci email dell'utente da modificare: ");
-        var ricercaEmail = Console.ReadLine();
+        string? ricercaEmail = Console.ReadLine();
 
-        List<string> righe = new List<string>(File.ReadAllLines(percorsoFile));
-        List<Contatto> contatti = new List<Contatto>();
-        foreach (var riga in righe)
+        List<Contatto> contattiEsistenti = CaricaRubrica();
+
+        foreach (var riga in contattiEsistenti)
         {
-            var colonne = riga.Split(',');
-            var contatto = new Contatto(colonne[0], colonne[1], colonne[2], colonne[3]);
-            contatti.Add(contatto);
-        }
-
-        for (int i = 0; i < contatti.Count; i++)
-        {
-            var contatto = contatti[i];
-
-            if (contatto.GetEmail() == ricercaEmail)
+            if (riga.GetEmail() == ricercaEmail)
             {
                 emailTrovata = true;
                 Console.Write("Inserisci nuovo nome: ");
@@ -88,16 +83,22 @@ public class Rubrica
                 Console.Write("Inserisci nuovo numero di telefono: ");
                 string? nuovoTelefono = Console.ReadLine();
 
-                contatti[i] = new Contatto(nuovoNome, nuovoCognome, nuovaEmail, nuovoTelefono);
+                if (string.IsNullOrEmpty(nuovoNome)) nuovoNome = riga.Nome;
+                if (string.IsNullOrEmpty(nuovoCognome)) nuovoCognome = riga.Cognome;
+                if (string.IsNullOrEmpty(nuovaEmail)) nuovaEmail = riga.Email;
+                if (string.IsNullOrEmpty(nuovoTelefono)) nuovoTelefono = riga.Telefono;
 
-                var nuovoContatto = contatti.Select(c => c.Get()).ToList();
-                File.WriteAllLines(percorsoFile, nuovoContatto);
+                riga.Nome = nuovoNome;
+                riga.Cognome = nuovoCognome;
+                riga.Email = nuovaEmail;
+                riga.Telefono = nuovoTelefono;
 
-                Console.WriteLine("\nContatto modificato correttamente");
+                SalvaRubrica(contattiEsistenti);
+                Console.WriteLine("Contatto modificato con successo!");
                 break;
+
             }
         }
-
         if (!emailTrovata)
         {
             Console.WriteLine("Email non trovata");
@@ -105,52 +106,29 @@ public class Rubrica
     }
     public static void EliminaContatto()
     {
-        string percorsoFile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\diecimila.csv";
-        bool emailTrovata = false;
         Console.Write("Inserisci email dell'utente da eliminare: ");
         string? ricercaEmail = Console.ReadLine();
 
-        List<string> righe = new List<string>(File.ReadAllLines(percorsoFile));
+        List<Contatto> contattiEsistenti = CaricaRubrica();
 
-        List<Contatto> contatti = new List<Contatto>();
-
-        foreach (var riga in righe)
+        foreach (var riga in contattiEsistenti)
         {
-            var colonne = riga.Split(',');
-            var contatto = new Contatto(colonne[0], colonne[1], colonne[2], colonne[3]);
-            contatti.Add(contatto);
-        }
-        for (int i = 0; i < contatti.Count; i++)
-        {
-            var contatto = contatti[i];
-
-            if (contatto.GetEmail() == ricercaEmail)
+            if (riga.GetEmail() == ricercaEmail)
             {
-                emailTrovata = true;
-                Console.WriteLine($"Sei sicuro di voler eliminare l'utente {ricercaEmail}? (s/n)");
+                contattiEsistenti.Remove(riga);
 
-                string? risposta = Console.ReadLine();
-
-                if (risposta.ToLower() == "s")
-                {
-                    
-                    contatti.RemoveAt(i);
-                    var nuoveRighe = contatti.Select(c => c.Get()).ToList();  // Usa il metodo Get per ottenere la rappresentazione CSV
-                    File.WriteAllLines(percorsoFile, nuoveRighe);
-                    Console.WriteLine("\nContatto eliminato correttamente");
-                    break;
-                }
+                SalvaRubrica(contattiEsistenti);
+                Console.WriteLine("Contatto eliminato con successo!");
+                break;
+            } else
+            {
+                Console.WriteLine("Email non trovata");
+                break;
             }
-        }
-
-        if (!emailTrovata)
-        {
-            Console.WriteLine("Email non trovata");
         }
     }
     public static void EliminaRubrica()
-    {
-        string percorsoFile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\diecimila.csv";
+    {        
         Console.WriteLine("Sei sicuro di voler eliminare la rubrica? (s/n)");
         string? EliminazioneRubrica = Console.ReadLine();
         if (EliminazioneRubrica.ToLower() == "s")
@@ -165,68 +143,32 @@ public class Rubrica
         }
     }
     public static void EsportaRubrica()
-    {
-        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\diecimila.csv";
+    {        
         string copyPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\";
         Console.WriteLine("Inserisci nome nuovo registro");
         var fileCopy = Console.ReadLine();
-        File.Copy(folderPath, copyPath + fileCopy + ".csv");
+        File.Copy(percorsoFile, copyPath + fileCopy + ".csv");
 
     }
     public static void ImportaRubrica()
     {
-        string percorsoFile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\diecimila.csv";
-        bool emailTrovata = false;
-        var ricercaFile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\";
-        Console.Write("Inserisci nome file da importare: ");
-        var fileImportato = ricercaFile + Console.ReadLine() + ".csv";
-
-
-        List<string> righe = new List<string>(File.ReadAllLines(percorsoFile));
-        var righeNuove = File.ReadAllLines(fileImportato).ToList();
-
-        for (int y = 0; y < righeNuove.Count; y++)
-        {
-            var colonne = righeNuove[y].Split(',');
-            bool emailDuplicata = false;
-
-            for (int i = 0; i < righe.Count; i++)
-            {
-                var colonne2 = righe[i].Split(',');
-
-
-                if (colonne2[2].Trim() == colonne[2].Trim())
-                {
-                    emailDuplicata = true;
-                    Console.WriteLine($"{colonne[2].Trim()} esiste già. Vuoi sostituirlo con {colonne2[0]},{colonne2[1]} ? (s/n)");
-                    var rispostaUtente = Console.ReadLine();
-                    if (rispostaUtente.ToLower() == "s")
-                    {
-                        righe.Remove(colonne[0] + colonne[1] + colonne[2] + colonne[3]);
-                        righeNuove.Add(colonne[0] + colonne[1] + colonne[2] + colonne[3]);
-                        File.WriteAllLines(percorsoFile, righeNuove);
-                        break;
-                    }
-                    else if (rispostaUtente.ToLower() == "n")
-                    {
-                        Console.WriteLine("Okay.");
-                        break;
-                    }
-                    else if (!emailDuplicata)
-                    {
-                        File.AppendAllLines(percorsoFile, righeNuove);
-                        break;
-                    }
-
-                }
-            }
-        }
 
     }
     public static void VisualizzaContatti()
     {
-        string percorsoFile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\diecimila.csv";
-        Console.WriteLine("\n" + File.ReadAllText(percorsoFile));
+
+        if (contatti.Count == 0)
+        {
+            Console.WriteLine("Non ci sono contatti nella rubrica.");
+        }
+        else
+        {
+            foreach (var contatto in contatti)
+            {
+                Console.WriteLine($"{contatto.Nome}, {contatto.Cognome}, {contatto.Email}, {contatto.Telefono}");
+            }
+        }
+
     }
 }
 
